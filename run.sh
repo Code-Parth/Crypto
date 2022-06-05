90f1d53484afce84ea1f1a8270b1d46aeaabe26f
Parmar Parth <84669955+Code-Parth@users.noreply.github.com>
2022-06-05 11:00:59 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
-sudo -i
-apt-get update
-apt install cmake build-essential libuv1-dev libssl-dev libtool automake autoconf
-apt-get update
+sudo apt-get update -y
+sudo apt install cmake build-essential libuv1-dev libssl-dev libtool automake autoconf -y
+sudo apt-get update -y
 git clone https://github.com/open-mpi/hwloc.git
 cd hwloc
 ./autogen.sh
 ./configure
 make
-make install
+sudo make install
 cd ..
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
